9c5ec85000ef2c0524ea742cdf70265c5b2db2e6
py9245 <py9245@naver.com>
2026-05-09 20:24:52 +0900

    Fix Cloudflare worker build output dir

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,11 @@ import { PHASE_DEVELOPMENT_SERVER } from "next/constants.js";
 
 export default function createNextConfig(phase) {
   const isDevServer = phase === PHASE_DEVELOPMENT_SERVER;
-  const distDir = isDevServer ? ".next-dev" : ".next-build";
 
   /** @type {import('next').NextConfig} */
   return {
     reactStrictMode: true,
-    distDir,
+    ...(isDevServer ? { distDir: ".next-dev" } : {}),
     experimental: {
       webpackBuildWorker: false,
     },
